52564ce7306d619b52ddf859d69f9471579a23af
Shubhwithai <shubham@buildfastwithai.com>
2024-05-13 19:11:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,10 +31,10 @@ def input_image_setup(uploaded_file):
 ## Initialize our streamlit app
 st.set_page_config(page_title="calorie Advisor ")
 st.header("Calorie Advisor App")
-st.header("Upload a image of Your Meal/Breakfast/Dinner")
+st.write('Upload a image of Your Meal/Breakfast/Dinner')
 
 
-uploaded_file = st.file_uploader("Upload a image of Your Meal/Breakfast/Dinner.....", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Upload a image.....", type=["jpg", "jpeg", "png"])
 image = ""
 
 if uploaded_file is not None:
